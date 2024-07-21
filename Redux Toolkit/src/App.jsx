e3a528275f235edbd20504cb5bc2d6a669142cd4
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-21 21:27:10 +0530

    redux toolkit by hitesh chaudhary todo making

---
+++ Redux Toolkit/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddTodo from "./Components/AddTodo";
+import Todos from "./Components/Todos";
+function App() {
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
